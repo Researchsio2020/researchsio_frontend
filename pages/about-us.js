85ae3b44f34465c7eda485394e40f24a3f5da512
Researchsio2020 <125734644+Researchsio2020@users.noreply.github.com>
2023-02-28 14:59:56 +0600

    Update about-us.js

--- pages/about-us.js
+++ pages/about-us.js
@@ -22,7 +22,7 @@ function AboutUs1() {
           >
             <div className="container">
               <div className="dlab-bnr-inr-entry">
-                <h1>About us 1</h1>
+                <h1>About Us</h1>
                 {/* <!-- Breadcrumb Row --> */}
                 <nav aria-label="breadcrumb" className="breadcrumb-row">
                   <ul className="breadcrumb">
@@ -32,7 +32,7 @@ function AboutUs1() {
                       </Link>
                     </li>
                     <li className="breadcrumb-item active" aria-current="page">
-                      About us
+                      About Us
                     </li>
                   </ul>
                 </nav>
